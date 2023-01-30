7070e798a806df0dfed6d9b2350df2c8bbef63bf
Viktor <viktormmeliassen@gmail.com>
2023-01-30 22:41:53 +0100

    Dialogue system version 2 + more art assets

--- Frost&Snow/Assets/Scripts/Viktor/Dialogue/DialogueManager.cs
+++ Frost&Snow/Assets/Scripts/Viktor/Dialogue/DialogueManager.cs
@@ -4,19 +4,52 @@ using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
+using UnityEngine.SearchService;
+using UnityEngine.UI;
+using UnityEditor.U2D.Path.GUIFramework;
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("Params")]
+    [SerializeField] private float typingSpeed = 0.04f;
+
 
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
+
+    [SerializeField] private GameObject continueIcon;
+
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+
+    [SerializeField] private Animator portraitAnimator;
+
+    
+
+    private Animator layoutAnimator;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
 
     private Story currentStory;
 
     public bool dialogueIsPlaying { get; private set; } //only want outside scripts to read value and not be able to modify it
 
+    private bool canContinueToNextLine = false;
+
+    private bool canSkip = false;
+
+    private bool submitSkip;
+
+    private Coroutine displayLineCoroutine;
+
     private static DialogueManager instance;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait"; //use animation to switch portrait states
+    private const string LAYOUT_TAG = "layout";
 
     private void Awake()
     {
@@ -36,17 +69,34 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+
+        //get the layout aniator
+        layoutAnimator = dialoguePanel.GetComponent<Animator>();
+
+        //get all of the choices text
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            submitSkip = true;
+        }
+
         if (!dialogueIsPlaying) //return right away if dialogue isn't playing
         {
             return;
         }
 
         //handle continuing to the next line in the dialogue when submit is pressed
-        if (Input.GetKeyDown(KeyCode.E))
+        if (canContinueToNextLine && Input.GetKeyDown(KeyCode.E))
         {
             ContinueStory();
         }
@@ -57,6 +107,11 @@ public class DialogueManager : MonoBehaviour
         currentStory = new Story(inkJSON.text);
         dialogueIsPlaying = true;
         dialoguePanel.SetActive(true);
+
+        //reset portrait, Layout and speaker
+        displayNameText.text = "???";
+        portraitAnimator.Play("default");
+        layoutAnimator.Play("Right");
 
         ContinueStory();
     }
@@ -69,15 +124,176 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = "";
     }
 
+    private IEnumerator CanSkip()
+    {
+        canSkip = false; //Making sure the variable is false.
+        yield return new WaitForSeconds(0.05f);
+        canSkip = true;
+    }
+
     private void ContinueStory()
     {
         if (currentStory.canContinue) // If there is dialogue to be played
         {
-            dialogueText.text = currentStory.Continue();
+            //stops coroutines from running at the same time, creating typewriter bug
+            if (displayLineCoroutine != null) 
+            {
+                StopCoroutine(displayLineCoroutine);
+            }
+            //set text for the current dialogue line
+            displayLineCoroutine = StartCoroutine(DisplayLine(currentStory.Continue()));
+            
+            //handle tags
+            HandleTag(currentStory.currentTags);
         }
         else
         {
             ExitDialogueMode();
         }
+    }
+
+    private IEnumerator DisplayLine(string line)
+    {
+        //empty the dialogue text so the previous line is no longer showing
+        dialogueText.text = "";
+
+        //hide items while text is typing
+        continueIcon.SetActive(false);
+        HideChoices();
+
+        submitSkip = false;
+        canContinueToNextLine = false;
+
+        bool isAddingRichTextTag = false;
+
+        StartCoroutine(CanSkip());
+        
+
+        //Display each letter one at a time
+        foreach (char letter in line.ToCharArray())
+        {
+            //if E is pressed, finish up displaying the line right awway
+            if (canSkip && submitSkip)
+            {
+                dialogueText.text = line;
+                break;
+            }
+
+            //check for rich text tag, if found, add it without waiting
+            if (letter == '<' || isAddingRichTextTag)
+            {
+                isAddingRichTextTag = true;
+                dialogueText.text += letter;
+                if (letter == '>')
+                {
+                    isAddingRichTextTag = false;
+                }
+            } 
+            else //if not rich text, add the next letter and wait a small time
+            {
+                dialogueText.text += letter;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+
+            
+        }
+
+        //actions to take after the entire line has finished displaying
+        continueIcon.SetActive(true);
+        //display choices, if any, for this dialogue line
+        DisplayChoices();
+
+        canContinueToNextLine = true;
+        canSkip = false;
+    }
+
+    private void HideChoices()
+    {
+        foreach (GameObject choiceButton in choices)
+        {
+            choiceButton.SetActive(false);
+        }
+    }
+
+    private void HandleTag(List<string> currentTags)
+    {
+        //loop through each tag and handle it accordingly
+        foreach (string tag in currentTags)
+        {
+            //parse the tag
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            //handle the tag
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    Debug.Log("speaker=" + tagValue);
+                    break;
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    Debug.Log("speaker=" + tagValue);
+                    break;
+                case LAYOUT_TAG:
+                    layoutAnimator.Play(tagValue);
+                    Debug.Log("speaker=" + tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        //defensive check to make sure our UI can support the number of choices coming in
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support. Number of choices given: " + currentChoices.Count);
+        }
+
+        int index = 0;
+        //enable and initialize the choices up to the amount of choices for this line of dialogue
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        //go through the remaining choices the UI supports and make sure they're hidden
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        /*StartCoroutine(SelectFirstChoice());*/
+    }
+
+   /*private IEnumerator SelectFirstChoice()
+    {
+        //Event System requires we clear it first, then wait
+        //for at least one frame before we set the current selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }*/
+
+    public void MakeChoice(int choiceIndex)
+    {
+        if (canContinueToNextLine)
+        {
+            currentStory.ChooseChoiceIndex(choiceIndex);
+            ContinueStory();
+        }
+        
     }
 }
--- Frost&Snow/Assets/Scripts/Viktor/Dialogue/Test.ink
+++
@@ -1,4 +0,0 @@
-This is a test!
-Here's another line!
-Another line now
-Hey
