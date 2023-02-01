b45dccf6dfa8c413834a2fe1bcd379c509ef4ff3
Tony H.Vu <tony.v95@hotmail.com>
2023-02-01 11:33:38 +0100

    Fixed more collider bug + lvl loader

--- Frost&Snow/Assets/Scripts/Tony/PuzzleMechanics/SnowPickUp.cs
+++ Frost&Snow/Assets/Scripts/Tony/PuzzleMechanics/SnowPickUp.cs
@@ -6,7 +6,7 @@ public class SnowPickUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Snow"))
+        if (collision.CompareTag("Snow") || collision.CompareTag("Frost"))
         {
             print("Snow food picked up!");
             Destroy(gameObject);
