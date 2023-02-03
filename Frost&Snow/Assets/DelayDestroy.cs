d847fde90aa1194011628643e090308f70366e5c
Tony H.Vu <tony.v95@hotmail.com>
2023-02-03 10:32:58 +0100

    Fixed text on levels

--- Frost&Snow/Assets/DelayDestroy.cs
+++ Frost&Snow/Assets/DelayDestroy.cs
@@ -7,7 +7,7 @@ public class DelayDestroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("DelayDestroyText", 30f); 
+        Invoke("DelayDestroyText", 60f); 
     }
 
 
