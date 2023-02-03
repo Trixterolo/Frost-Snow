a8de8cf35cb148e69fb168cad747c91066926c2c
Tony H.Vu <tony.v95@hotmail.com>
2023-02-03 02:08:12 +0100

    Added idle Wolf, fixed lvl and animation bugs.

--- Frost&Snow/Assets/Scripts/Jonas/CameraScript.cs
+++ Frost&Snow/Assets/Scripts/Jonas/CameraScript.cs
@@ -10,14 +10,16 @@ public class CameraScript : MonoBehaviour
     
     void Update()
     {
-        Vector3 cameraPosition = transform.position;
-        if (player.transform.position.x > -5)
-            cameraPosition.x = player.position.x;
-        if (player.transform.position.y < -5)
-            cameraPosition.y = player.position.y;
+            Vector3 cameraPosition = transform.position;
+            if (player.transform.position.x > -5)
+                cameraPosition.x = player.position.x;
+            if (player.transform.position.y < -5)
+                cameraPosition.y = player.position.y;
 
-        cameraPosition.z = -11;
+            cameraPosition.z = -11;
 
-        transform.position = cameraPosition;
+            transform.position = cameraPosition;
+
+
     }
 }
